887bdd1215b07cde8646eeebc6542915537c6a82
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-02-06 18:38:29 +0900

    add: 調整さん日程メーカー

---
+++ choseisan.rb
@@ -0,0 +1,17 @@
+# 【内容】
+# 調整さんの日程メーカー。
+# Usage:
+#   9,10行目に日付と時間帯を入力して以下のコマンドを実行する。
+#   ruby choseisan.rb | pbcopy
+
+require "date"
+
+日程 = %w(12 13 14 15 16)
+時間帯 = %w(13:00~ 14:00~ 15:00~ 16:00~ 17:00~)
+
+for d in 日程.map{Date.parse(_1)}
+    for t in 時間帯
+        y="日月火水木金土"[d.wday]
+        puts d.strftime"%-m/%-d(#{y}) #{t}"
+    end
+end
